f626cc2fa51ef248986624c2519bca18806440f3
pavangkumar2 <pavangkumar2@gmail.com>
2017-07-06 01:28:09 +0530

    modified java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=10;i++){
+		for(int i=1;i<=11;i++){
 			System.out.println("Hello world " + i);
 		}
 
-	}
+	
 
 }
